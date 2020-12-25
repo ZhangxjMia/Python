5751c8ffafc7c156a454004c7c615f0f88e481b7
Mia (Xiaojia) Zhang <63559049+ZhangxjMia@users.noreply.github.com>
2020-12-25 00:22:00 -0800

    Clustering for dataset exploration

--- Clustering for dataset exploration.py
+++ Clustering for dataset exploration.py
@@ -1,16 +1,16 @@
-Unsupervised Learning
-* Unsupervised learning finds patterns in data
-    * clustering customers by their purchases (clustering)
-    * compressing the data using purchase patterns (dimension reduction)
+#Unsupervised Learning
+#* Unsupervised learning finds patterns in data
+#    * clustering customers by their purchases (clustering)
+#    * compressing the data using purchase patterns (dimension reduction)
 
-Supervised Learning vs Unsupervised Learning
-* Supervised learning finds patterns for a prediction task
-    * classify tumors as benign or cancerous (labels)
-* Unsupervised learning finds patterns in data but without a specific prediction task in mind (pure pattern discovery)
+#Supervised Learning vs Unsupervised Learning
+#* Supervised learning finds patterns for a prediction task
+#    * classify tumors as benign or cancerous (labels)
+#* Unsupervised learning finds patterns in data but without a specific prediction task in mind (pure pattern discovery)
 
 
 
-Clustering 2D points
+#Clustering 2D points
 # Import KMeans
 from sklearn.cluster import KMeans
 
@@ -38,7 +38,7 @@ print(labels)
 
 
 
-Inspect your clustering
+#Inspect your clustering
 # Import pyplot
 import matplotlib.pyplot as plt
 
@@ -62,17 +62,17 @@ plt.show()
 
 ￼
 
-Evaluating a clustering
+#Evaluating a clustering
 
-Measuring clustering quality
-* Using only samples and their cluster labels
-* A good clustering has tight clusters
-* Samples in each cluster bunched together
+#Measuring clustering quality
+#* Using only samples and their cluster labels
+#* A good clustering has tight clusters
+#* Samples in each cluster bunched together
 
-Inertia measures clustering quality
-* Measures how spread out the clusters are (lower is better)
-* Measures how far samples are from their centroids
-* After fit(), available as attribute inertia_
+#Inertia measures clustering quality
+#* Measures how spread out the clusters are (lower is better)
+#* Measures how far samples are from their centroids
+#* After fit(), available as attribute inertia_
     * from sklearn.cluster import KMeans
     * model = KMeans(n_clusters = 3)
     * model.fit(samples)
@@ -80,7 +80,7 @@ Inertia measures clustering quality
 
 
 
-How many clusters of grain?
+#How many clusters of grain?
 ks = range(1, 6)
 inertias = []
 
@@ -103,7 +103,7 @@ plt.show()
 
 ￼
 
-Evaluating the grain clustering
+#Evaluating the grain clustering
 import pandas as pd
 from sklearn.cluster import KMeans
 
@@ -123,53 +123,52 @@ ct = pd.crosstab(df['labels'], df['varieties'])
 # Display ct
 print(ct)
 
-Output:
-varieties  Canadian  Kama  Rosa (wheat)
-labels                                           
-0             0        1          60
-1            68        9           0
-2             2       60          10
+#Output:
+#varieties  Canadian  Kama  Rosa (wheat)
+#labels                                           
+#0             0        1          60
+#1            68        9           0
+#2             2       60          10
 
 
 
-Transforming features for better clusterings
+#Transforming features for better clusterings
 
-Piedmont wines dataset
-* 178 samples from 3 distinct varieties if red wine: Barolo, Grignolino, and Barbera
-* Features measure chemical composition e.g. alcohol content
-* Visual properties like ‘color intensity’
+#Piedmont wines dataset
+#* 178 samples from 3 distinct varieties if red wine: Barolo, Grignolino, and Barbera
+#* Features measure chemical composition e.g. alcohol content
+#* Visual properties like ‘color intensity’
 
-Clustering the wines:
+#Clustering the wines:
 from sklearn.cluster import KMeans
 model = KMeans(n_clusters = 3)
 labels = model.fit_predict(samples)
 
-Clusters vs. varieties
+#Clusters vs. varieties
 df = pd.DataFrame({‘labels’ : labels,
 				     ‘varieties’ : varieties})
 ct = pd.crosstab(df[‘labels’], df[‘varieties’])
 
-Output:
+#Output:
+#varieties		Barbera 	Barolo		Grignolino
+#labels
+#0			29		13		20
+#1			0		46		1
+#2			19		0		50
 
-varieties		Barbera 	Barolo		Grignolino
-labels
-0				29			13			20
-1				0			46			1
-2				19			0			50
+#The KMeans clusters don’t correspond well with the wine varieties
+#The problem is:
+#* The wine features have very different variances
+#* Variance of a feature measures spread of its values	
 
-The KMeans clusters don’t correspond well with the wine varieties
-The problem is:
-* The wine features have very different variances
-* Variance of a feature measures spread of its values	
-
-StandardScaler
-* In KMeans: feature variance = feature influence
-* To give every feature a chance, the data needs to be transformed so that features have equal variance
-* StandardScaler transforms each feature to have mean 0 and variance 1
-* Features are said to be ‘standardized’
+#StandardScaler
+#* In KMeans: feature variance = feature influence
+#* To give every feature a chance, the data needs to be transformed so that features have equal variance
+#* StandardScaler transforms each feature to have mean 0 and variance 1
+#* Features are said to be ‘standardized’
 ￼
 
-sklearn StandardScaler
+#sklearn StandardScaler
 from sklearn.preprocessing import StandardScaler
 scaler = StandardScaler()
 scaler.fit(samples)
@@ -178,7 +177,7 @@ samples_scaled = scaler.transform(samples)
 
 
 
-Pipeline combine multiple steps
+#Pipeline combine multiple steps
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 scaler = StandardScaler()
@@ -188,16 +187,16 @@ pipeline = make_pipeline(scaler, kmeans)
 pipeline.fit(samples)
 labels = pipeline.predict(samples)
 
-with feature standardization:
-varieties		Barbera 	Barolo		Grignolino
-labels
-0				0			59			3
-1			    48			0			3
-2				0			0			65
+#with feature standardization:
+#varieties		Barbera 	Barolo		Grignolino
+#labels
+#0			0		59			3
+#1			48		0			3
+#2			0		0			65
 
 
 
-Scaling fish data for clustering
+#Scaling fish data for clustering
 # Perform the necessary imports
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import StandardScaler
@@ -214,7 +213,7 @@ pipeline = make_pipeline(scaler, kmeans)
 
 
 
-Clustering the fish data
+#Clustering the fish data
 # Import pandas
 import pandas as pd
 
@@ -233,18 +232,19 @@ ct = pd.crosstab(df['labels'], df['species'])
 # Display ct
 print(ct)
 
-Outout:
-    species  Bream  Pike  Roach  Smelt
-    labels                            
-    0          0     0      0      13
-    1          33    0      1       0
-    2          0     17     0       0
-    3          1     0     	19      1
+#Outout:
+#    species  Bream  Pike  Roach  Smelt
+#    labels                            
+#    0          0     0      0      13
+#    1          33    0      1       0
+#    2          0     17     0       0
+#    3          1     0     19       1
 
 
 
-Clustering stocks using KMeans
-While StandardScaler() standardizes features (such as the features of the fish data from the previous exercise) by removing the mean and scaling to unit variance, Normalizer() rescales each sample - here, each company's stock price - independently of the other.
+#Clustering stocks using KMeans
+#While StandardScaler() standardizes features (such as the features of the fish data from the previous exercise) by removing the mean and scaling to unit variance, 
+#Normalizer() rescales each sample - here, each company's stock price - independently of the other.
 
 # Import Normalizer
 from sklearn.preprocessing import Normalizer
